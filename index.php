b1ab8ea73760fc1d86811cacd1047344e05f2af9
Krypto Dev <174056579+PETTER14171@users.noreply.github.com>
2025-08-18 08:05:13 -0600

    Se ajustan utilidades de sass

--- index.php
+++ index.php
@@ -3,10 +3,12 @@ require 'includes/funciones.php';
 incluirTemplate ('header');
 ?>
 
-<main>
-    <h2>Bienvenido al Sistema de Gestión de Tickets</h2>
-    <p>Accede al portal de atención para agentes, técnicos y administradores. Gestiona reportes, consulta guías de solución o da seguimiento a tus incidencias con eficiencia y rapidez.</p>
-    <a href="login.php" class="btn-login">Iniciar sesión</a>
+<main class="seccion">
+    <div class="contenido-centrado">
+        <h2>Bienvenido al Sistema de Gestión de Tickets</h2>
+        <p>Accede al portal de atención para agentes, técnicos y administradores. Gestiona reportes, consulta guías de solución o da seguimiento a tus incidencias con eficiencia y rapidez.</p>
+        <a href="login.php" class="btn-login">Iniciar sesión</a>
+    </div>
 </main>
 
 <footer>
